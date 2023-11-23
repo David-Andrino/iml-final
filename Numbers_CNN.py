ef3654fb819f39ed55fe8cd53fb313fbadd51bff
SoraSpades <davidandrino2002@gmail.com>
2023-11-23 13:10:10 +0100

    Add tabbing

--- Numbers_CNN.py
+++ Numbers_CNN.py
@@ -3,8 +3,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 # Download the training data into the "data" folder
-train = datasets.MNIST(root="data", download=True, train=True,
-transform=ToTensor())
+train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 
 # Group the training dataset into batches of size 32
 train_batches = DataLoader(dataset=train, batch_size=32)
@@ -21,11 +20,12 @@ for batch in train_batches:
     data, labels = batch
 
     # Check what are "data" and "labels"
-    print(f"Type of 'data': {type(data)}")
-    print(f"Type of 'labels': {type(labels)}")
-    # Check the shape of "data" and "labels"
-    print(f"Shape of data Tensor from current batch: {data.shape}")
-    print(f"Shape of first image Tensor from current batch: {data[0].shape}")
-    print(f"Shape of labels Tensor from current batch: {labels.shape}")
-    print(f"Labels included in the 'labels' Tensor from current batch: {labels}")
+    print(f"    Type of 'data': {type(data)}")
+    print(f"    Type of 'labels': {type(labels)}")
+    # Check     the shape of "data" and "labels"
+    print(f"    Shape of data Tensor from current batch: {data.shape}")
+    print(f"    Shape of first image Tensor from current batch: {data[0].shape}")
+    print(f"    Shape of labels Tensor from current batch: {labels.shape}")
+    print(f"    Labels included in the 'labels' Tensor from current batch: {labels}")
     # Use "break" to exit the for loop
+    break

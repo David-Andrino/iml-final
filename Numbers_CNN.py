aad09c37def1de06630a3af330faf372a1216033
r-agus <r.agustin@alumnos.upm.es>
2023-12-19 17:10:04 +0100

    excercises

--- Numbers_CNN.py
+++ Numbers_CNN.py
@@ -1,66 +1,78 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-
+from matplotlib import pyplot as plt
+import os
 # Download the training data into the "data" folder
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 
 # Group the training dataset into batches of size 32
 train_batches = DataLoader(dataset=train, batch_size=32)
 
-''' Exercise 1
-print(f"Number of created batches: {len(train_batches)}")
-for batch in train_batches:
-    print(f"Type of 'batch': {type(batch)}")
-    data, labels = batch
-    print(f"    Type of 'data': {type(data)}")
-    print(f"    Type of 'labels': {type(labels)}")
-    print(f"    Shape of data Tensor from current batch: {data.shape}")
-    print(f"    Shape of first image Tensor from current batch: {data[0].shape}")  ## Array shape: 28x28 1D
-    print(f"    Shape of labels Tensor from current batch: {labels.shape}")
-    print(f"    Labels included in the 'labels' Tensor from current batch: {labels}")
-    break
-'''
+# #  Exercise 1
+# print(f"Number of created batches: {len(train_batches)}")
+# for batch in train_batches:
+#     print(f"Type of 'batch': {type(batch)}")
+#     data, labels = batch
+#     print(f"    Type of 'data': {type(data)}")
+#     print(f"    Type of 'labels': {type(labels)}")
+#     print(f"    Shape of data Tensor from current batch: {data.shape}")
+#     print(f"    Shape of first image Tensor from current batch: {data[0].shape}")
+#     print(f"    Shape of labels Tensor from current batch: {labels.shape}")
+#     print(f"    Labels included in the 'labels' Tensor from current batch: {labels}")
+#     break
 
-''' Draw training images
-import matplotlib.pyplot as plt
 
-for batch in train_batches:
-    plt.figure()
-    data, labels = batch
-    for i in range(6):
-        plt.subplot(2, 3, i + 1)
-        plt.tight_layout()
-        plt.imshow(data[i][0], cmap="gray", interpolation="none")
-        plt.title(f"Ground Truth label: {labels[i]}")
-        plt.xticks([])
-        plt.yticks([])
+# # Draw training images
+# for batch in train_batches:
+#     plt.figure()
+#     data, labels = batch
+#     for i in range(6):
+#         plt.subplot(2, 3, i + 1)
+#         plt.tight_layout()
+#         plt.imshow(data[i][0], cmap="gray", interpolation="none")
+#         plt.title(f"Ground Truth label: {labels[i]}")
+#         plt.xticks([])
+#         plt.yticks([])
 
-    plt.show()
-    plt.close()
-    break
-'''
+#     plt.show()
+#     plt.close()
+#     break
 
-# Define the Convolutional Neural Network architecture
+from math import floor
+
+# PARAMETER CALCULATION
+out = [128, 256, 512]
+# out = [64, 128, 256]
+# out = [32, 64, 128]
+kernels = [(3, 3), (3, 3), (3, 3)]
+
+padding = 0
+dilation = 1
+stride = 1
+
+H = 28
+train = True # True for training the CNN, False for loading from disk
+evaluate = True # True for running the prediction on the whole database and calculate accuracy
+img_idx = 118 # If evaluate is false, predict this single image
+num_epochs = 5
+
+for i in range(0, 3):
+    H = floor((H + 2*padding - dilation*(kernels[i][0]-1) - 1)/stride + 1)
+
+in_features = out[2]*H*H
+
+print(f"In Features: {in_features}")
+
+
+# PREDICTION
 import torch
 import math
 # CNN MNIST classifier
 class DigitClassifier(torch.nn.Module):
     def __init__(self):
         super().__init__()
-
-        padding = 0
-        dilation = 1
-        stride = 1
-
-        H = math.floor((28 + 2 * padding - dilation * (3 - 1) - 1)/stride + 1)
-        
-        for i in range(2):
-            H = math.floor((H + 2*padding - dilation * (3 - 1) - 1)/stride + 1)
-            print(str(i+2) + ": " + str(H))
-
- 
-
+        gpu1 = torch.device
         self.model = torch.nn.Sequential(
             torch.nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3)),
             torch.nn.ReLU(),
@@ -77,48 +89,75 @@ class DigitClassifier(torch.nn.Module):
         return self.model(x)
 
 cnn = DigitClassifier()
-# for batch in train_batches:
-#     X, y = batch
-#     y_pred = cnn(X)
-#     exit()
 
-# TRAINING PROCEDURE
-# Instance of the loss function and optimizer
-opt = torch.optim.Adam(cnn.parameters(), lr=1e-3)
-loss_fn = torch.nn.CrossEntropyLoss()
+# TEST DATA
+test_data = datasets.MNIST(root="data", download=True, train=False, transform=ToTensor())
+test_batches = DataLoader(dataset=test_data, batch_size=32)
+cnn.eval()
 
+if train:
+    opt = torch.optim.Adam(cnn.parameters(), lr=1e-3)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    cnn.train(True)
 
-# Number of times to pass through every batch
-num_epochs = 10
-# Set the CNN to training mode
-cnn.train(True)
-for epoch in range(num_epochs):
-    for batch_idx, (X, y) in enumerate(train_batches):
-        # Pass the batch of images to obtain a prediction
-        y_pred = cnn(X)
-        # Compute the loss comparing the predictions
-        loss = loss_fn(input=y_pred, target=y)
-        # Perform backpropagation with the computed loss
-        # to compute the gradients
-        loss.backward()
-        # Update the weights with regard to the computed
-        # gradients to minimize the loss
-        opt.step()
-        # In each iteration we want to compute new gradients,
-        # that is why we set the gradients to 0
-        opt.zero_grad()
-        # Print to check the progress
-        if batch_idx % 50 == 0:
-            print(f"Train Epoch: {epoch} [{ batch_idx*len(X)}/{len(train_batches.dataset)} ({100.0 * batch_idx /len(train_batches):.0f}%)]\tLoss: {loss.item():.6f}")
-    print(f"Epoch: {epoch} loss is {loss.item()}")
+    for epoch in range(num_epochs):
+        for batch_idx, (X, y) in enumerate(train_batches):
+            y_pred = cnn(X)
+            loss = loss_fn(input=y_pred, target = y)
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
 
+            if batch_idx % 50 == 0:
+                print(
+                    f"Train Epoch: {epoch} [{ batch_idx *len(X)}/{len(train_batches.dataset)} ({100.0 * batch_idx / len(train_batches):.0f}%)]\tLoss: {loss.item():.6f}"
+                )
+        print(f"Epoch: {epoch} loss is {loss.item()}")
+    
+    with open("./cnn_model_state.pt", "wb") as f:
+        torch.save(cnn.state_dict(), f)
 
-from torch import save, load
-# Save the model state
-with open("./cnn_model_state.pt", "wb") as file:
-    save(cnn.state_dict(), file)
+else:
+    with open("./cnn_model_state.pt", "rb") as f:
+        cnn.load_state_dict(torch.load(f))
 
-# Load the model state
-with open("./cnn_model_state.pt", "rb") as file:
-    cnn.load_state_dict(load(file))
+if evaluate:
+    with torch.no_grad(): # Avoid calculating gradients
+        average_accuracy = []
+        # Predict the images in batches
+        for images, labels in test_batches:
+            # Predict the images with probabilities
+            test_output = cnn(images)
+            # For each image, take the highest probability
+            y_test_pred = torch.max(test_output, 1)[1].data.squeeze()
+            # Measure the accuracy
+            accuracy = (y_test_pred == labels).sum().item() / float(labels.size(0))
+            # print("VALIDATION SET ACCURACY: %.2f" % accuracy)
+            average_accuracy.append(accuracy)
+        # os.system("cls")
+        print(f"Out values: {out}")
+        print(f"Average Accuraccy: {sum(average_accuracy)/len(average_accuracy)}")
+else:
+    img_tensor, label = test_data[img_idx]
+    img_tensor = img_tensor.unsqueeze(0)
 
+    # Predict with the CNN
+    with torch.no_grad():  # Avoid calculating gradients
+
+        # Generate the prediction
+        img_pred = cnn(img_tensor)
+        pred_label = torch.argmax(img_pred)
+
+        # Plot the result
+        plt.figure()
+        plt.suptitle(f"Original label: {label}")
+        plt.title(f"Predicted label: {pred_label}")
+        # From the tensor extract only the image
+        plt.imshow(img_tensor[0, 0, :, :], cmap="gray")
+        # Remove the numbers in the axes
+        plt.xticks([])
+        plt.yticks([])
+        plt.savefig(f"./prediction_of_test_image_{img_idx}.png")
+
+        plt.show()
+    
